52ada9fab131a0ebda14a0ad7a0b1587052b21db
= <=>
2021-03-17 20:35:26 +0700

    contro

---
+++ contro.cpp
@@ -0,0 +1,27 @@
+
+
+// bai 3
+
+double* getSquare (double number)
+{
+    double *k= new double(number*number);
+    return k;
+}
+
+
+// bai 2
+double* getPointerToPi()
+{
+    double *pi=new double(3.14159);
+    return pi;
+}
+
+
+
+
+// bai 1
+int* getPointerToTen()
+{
+   int * p=new int(10);
+   return p;
+}
